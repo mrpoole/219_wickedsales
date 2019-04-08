1eed28ce282be1aa8362fcf0eeef79e0213ef611
Michelle Poole <michelle.poole.email@gmail.com>
2019-04-08 11:42:06 -0700

    added card

--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -6,11 +6,11 @@ class ProductDetails extends Component {
         details: null
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         const { params } = this.props.match;
         const resp = await axios.get(`/api/getproductdetails.php?productID=${params.product_id}`);
 
-        if(resp.data.success){
+        if (resp.data.success) {
             this.setState({
                 details: resp.data.productInfo
             });
@@ -21,22 +21,31 @@ class ProductDetails extends Component {
         }
     }
 
-    render(){
+    render() {
         console.log('Product Details:', this.state.details)
-        const {details} = this.state;
+        const { details } = this.state;
 
-        if(details === null){
+        if (details === null) {
             return <h1>Loading...</h1>
-        } else if(!details){
+        } else if (!details) {
             return <h1 className="center">NO PRODUCT FOUND</h1>
         }
 
-        const { description = "No description available", name } = details;
+        const { description = "No description available", name, image } = details;
 
-        return(
+        return (
             <div className="product-details">
-                <h1 className="center">{name}</h1>
-                <p>{description}</p>
+                <div className="row">
+                    <div className="col s12 m6 offset-m3">
+                        <div className="card blue-grey darken-1">
+                            <div className="card-content white-text">
+                                <span className="card-title">{name}</span>
+                                <img src={`/dist/${image}`}/>
+                                <p>{description}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
