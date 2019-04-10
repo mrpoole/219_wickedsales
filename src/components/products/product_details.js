979e58069436f6344f1eef10c6a6d3768b5ef4f4
Michelle Poole <michelle.poole.email@gmail.com>
2019-04-10 09:13:21 -0700

    commit

--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import ProductCarousel from './product_carousel';
-import { formatMoney } from '../../helpers';
-import MiscDetails from './misc_details';
 import ProductAdd from './product_add';
+import ProductCarousel from './product_carousel';
+import MiscDetails from './misc_details';
+import { formatMoney } from '../../helpers';
 
 class ProductDetails extends Component {
     state = {
         details: null
     }
 
-    async componentDidMount() {
-        const { params } = this.props.match;
-        const { params } = this.props.match;
-        const resp = await axios.get(`/api/getproductdetails.php?productID=${params.product_id}`);
+    componentDidMount(){
+        this.getDetails()
+    }
 
-        if (resp.data.success) {
+    async getDetails(){
+        const { params } = this.props.match;
+        // Call server to get product details
+
+        const resp = await axios.get(`/api/getproductdetails.php?productId=${params.product_id}`);
+
+        if(resp.data.success){
             this.setState({
                 details: resp.data.productInfo
             });
@@ -26,29 +31,28 @@ class ProductDetails extends Component {
         }
     }
 
-    render() {
+    render(){
         const { details } = this.state;
+        const { params } = this.props.match;
 
-        if (details === null) {
+        if(details === null){
             return <h1>Loading...</h1>
-        } else if (!details) {
-            return <h1 className="center">NO PRODUCT FOUND</h1>
+        } else if(!details){
+            return <h1 className="center">No Product Found</h1>
         }
 
-        const { description = "No description available", name, images, price, miscDetails } = details;
+        const { description = 'No description available', images, miscDetails, name, price } = details;
 
         return (
             <div className="product-details">
                 <h1 className="center">{name}</h1>
                 <div className="row">
                     <ProductCarousel images={images} />
-                    <div className="col s12 m4">
+                    <div className="col s12 m8">
                         <div className="right-align product-price">{formatMoney(price)}</div>
-
                         <ProductAdd productId={params.product_id}/>
-
                         <p>{description}</p>
-                        <MiscDetails details={miscDetails} />
+                        <MiscDetails details={miscDetails}/>
                     </div>
                 </div>
             </div>
